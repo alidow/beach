051fce2e40428b87a1d9faac59b2454b85d68ab6
alidow <alidow@gmail.com>
2025-10-14 18:14:58 -0400

    various

--- apps/beach-web/src/main.tsx
+++ apps/beach-web/src/main.tsx
@@ -10,6 +10,11 @@ if (!rootElement) {
   throw new Error('Root element #root not found');
 }
 
+if (typeof window !== 'undefined' && (window as any).__BEACH_TRACE) {
+  // eslint-disable-next-line no-console
+  console.info('[beach-web] version', __APP_VERSION__);
+}
+
 const RootComponent = isTerminalFirstShell() ? AppV2 : App;
 
 ReactDOM.createRoot(rootElement).render(
